77ef7dd472f3f2f750edb39ed35b8cd661c1c066
Alexssander Tavares <alexssander.tavares.dev@gmail.com>
2024-11-30 19:12:07 -0300

    feat: Adding Insert and Get methods for our ProductRepository and Test Class

--- Kotlin/practice2/src/DataSources/Repositories/ProductRepository.kt
+++ Kotlin/practice2/src/DataSources/Repositories/ProductRepository.kt
@@ -10,7 +10,7 @@ import java.sql.SQLTimeoutException
 
 class ProductRepository(private val testEnvironment: Boolean = false) {
 
-    private lateinit var db: Connection
+    private var db: Connection
 
     init {
         if (testEnvironment) {
@@ -26,14 +26,13 @@ class ProductRepository(private val testEnvironment: Boolean = false) {
         try {
             if (productName.length < 120 && category <= 3 || category > 0) {
                 val query: PreparedStatement =
-                    this.db.prepareStatement("INSERT INTO Product (str_name, i_quantity, i_category) values (${productName}, ${quantity}, ${category})")
+                    this.db.prepareStatement("INSERT INTO Product (str_name, i_quantity, i_category) VALUES ('${productName}', '${quantity}', '${category}');")
 
-                if (query.execute()) {
-                    rowsAffected += 1
-                    println("Insertion Success! Query ok: ${rowsAffected} row affected")
-                    db.close()
-                    return rowsAffected
-                }
+                query.execute()
+                rowsAffected += 1
+                println("Insertion Success! Query ok: ${rowsAffected} row affected")
+                db.close()
+                return rowsAffected
             } else {
                 throw IllegalArgumentException("Product Name must have length below 120 chars and above 0, category must be above 0 and below 3.")
             }
@@ -47,26 +46,28 @@ class ProductRepository(private val testEnvironment: Boolean = false) {
             e.printStackTrace()
             println("You must fulfill the right data requirements in order to register some data.")
             println("Product Name Size: ${productName.length} | Category: ${category}")
-
         }
         return rowsAffected
     }
 
     fun get(name: String): Product? {
+        var product: Product? = null
         try {
-            val query1: PreparedStatement = this.db.prepareStatement("SELECT * FROM Product;")
+            val query: PreparedStatement = this.db.prepareStatement("SELECT * FROM Product;")
 
             try {
-                val res: ResultSet = query1.executeQuery()
+                val res: ResultSet = query.executeQuery()
                 res.next() // moves cursor from row 0 to 1
                 try {
-                    while (res.getString("str_name") != name) {
-                        if (res.getString("str_name") == name) {
-                            val product =
-                                Product(res.getString("str_name"), res.getInt("i_quantity"), res.getInt("i_category"))
-                            return product
-                        }
+                    var res2: String = res.getString("str_name")
+                    while (res2 != name) {
                         res.next()
+                        res2 = res.getString("str_name")
+                    }
+
+                    if (res2 == name) {
+                        product = Product(res.getString("str_name"), res.getInt("i_quantity"), res.getInt("i_category"))
+                        return product
                     }
                 } catch (e: SQLException) {
                     println(e.message)
@@ -87,7 +88,7 @@ class ProductRepository(private val testEnvironment: Boolean = false) {
             e.printStackTrace()
             println("Closed connection.")
         }
-        return null
+        return product
     }
 
     /*fun delete(productName: String) : Int {
--- Kotlin/practice2/src/Test/ProductRepositoryTest.kt
+++ Kotlin/practice2/src/Test/ProductRepositoryTest.kt
@@ -2,8 +2,12 @@ package Test
 
 import DataSources.Data.DatabaseConnection
 import DataSources.Repositories.ProductRepository
+import Models.Product
+import junit.framework.TestCase.assertEquals
 import org.junit.After
 import org.junit.Before
+import org.junit.Test
+import org.junit.jupiter.api.fail
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 import java.sql.Connection
@@ -20,6 +24,30 @@ class ProductRepositoryTest {
 
     @After
     fun report(){
+
+    }
+
+    @Test
+    fun ShouldInsertRegistryInTestDatabaseAndReturnTheNumberOfAffectedRows(){
+        val product: Product = Product("TestDummy3", 5, 3)
+        var res: Int = 0
+        try {
+            res = this.repo.insert(product.name, product.quantity, product.category)
+            assertEquals(1, res)
+        } catch (e: Exception) {
+            fail("Failed to insert, Result: $res", e)
+        }
+    }
+
+    @Test
+    fun ShouldReturnAProductRegistryFromDatabase(){
+        var product: Product? = null
+        try {
+            product = this.repo.get("TestDummy3") ?: Product("Junk", 1, 1)
+            assertEquals("TestDummy3", product?.name)
+        } catch (e: Exception){
+            fail("Failed to get requested data, returned: ${product?.name}")
+        }
 
     }
 
